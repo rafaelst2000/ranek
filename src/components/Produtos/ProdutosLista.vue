a4bc6b355992347bb546b6485695568240aed0a3
Rafael Trevisan <rafaelst2000@gmail.com>
2020-10-19 19:08:23 -0300

    instalando o axios

---
+++ src/components/Produtos/ProdutosLista.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="produtos-container">
+    <div v-for="produto in produtos" :key="produto.id">
+      <img
+        v-if="produto.fotos.length > 0"
+        :src="produto.fotos[0].src"
+        :alt="produto.fotos[0].titulo"
+      />
+      <p class="preco">{{ produto.preco }}</p>
+      <h2 class="titulo">{{ produto.nome }}</h2>
+      <p class="descricao">{{ produto.descricao }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+import { api } from "@/services/services.js"
+export default {
+  data() {
+    return {
+      produtos: [],
+    }
+  },
+  created() {
+    this.getProdutos()
+  },
+  methods: {
+    getProdutos() {
+      api.get("/produto").then((r) => {
+        this.produtos = r.data
+      })
+    },
+  },
+}
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,27 @@
 <template>
-  <div></div>
+  <section>
+    <h1>👍 Compre ou Venda 👎</h1>
+    <ProdutosBuscar />
+    <ProdutosLista />
+  </section>
 </template>
 
 <script>
-export default {}
+import ProdutosBuscar from "@/components/Produtos/ProdutosBuscar.vue"
+import ProdutosLista from "@/components/Produtos/ProdutosLista.vue"
+export default {
+  components: {
+    ProdutosBuscar,
+    ProdutosLista,
+  },
+}
 </script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #87f;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
