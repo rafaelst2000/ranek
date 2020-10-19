7ff050cfd7351f07053698cf4ab3600e2d417443
Rafael Trevisan <rafaelst2000@gmail.com>
2020-10-19 19:35:38 -0300

    adicionando estilo a lista de produtos

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 export function serialize(obj) {
   let queryString = ""
-  for (let key in this.$route.query) {
-    queryString += `&${key}=${this.$route.query[key]}`
+  for (let key in obj) {
+    queryString += `&${key}=${obj[key]}`
   }
   return queryString
 }
